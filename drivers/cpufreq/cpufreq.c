929c0bb0566e202d2965dd1ea74baeed4b6caae7
Jarrod Worlitz <jarrod.worlitz@gmail.com>
2016-11-03 05:44:14 -0700

    angler: cpufreq: Lower hardlimit to 302400

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -34,9 +34,9 @@
 static unsigned int min_freq_hardlimit[8] = {0, 0};
 static unsigned int max_freq_hardlimit[8] = {0, 0};
 
-#define CONFIG_MSM_CPU_FREQ_MIN_GROUP1 384000
+#define CONFIG_MSM_CPU_FREQ_MIN_GROUP1 302400
 #define CONFIG_MSM_CPU_FREQ_MAX_GROUP1 1555200
-#define CONFIG_MSM_CPU_FREQ_MIN_GROUP2 384000
+#define CONFIG_MSM_CPU_FREQ_MIN_GROUP2 302400
 #define CONFIG_MSM_CPU_FREQ_MAX_GROUP2 1958400
 
 /* HACK: Prevent big cluster turned off when changing governor settings. */
