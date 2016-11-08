baad5e14205ef54dfd6f899513f7f9a527bd7504
Jarrod Worlitz <jarrod.worlitz@gmail.com>
2016-11-08 02:46:31 -0800

    MegatroN v0.70

--- megatron-build.sh
+++ megatron-build.sh
@@ -130,7 +130,7 @@ done
 
 # Kernel Details
 BASE_AK_VER="MegatroN"
-VER="v0.65"
+VER="v0.70"
 AK_VER="$BASE_AK_VER-$VER-$TC-$BUILD_DATE"
 
 while read -p "Do you want to clean out the old crusties (y/n)? " cchoice
